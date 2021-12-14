394041262a57de11060e9e65ec73cf957bc1193f
Joel Grant <joelmgrant@me.com>
2021-12-14 15:39:24 -0700

    Feat: Create function to add actor to a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
   def show
-    @movies = Movie.all
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    if params[:name]
+      @actor = Actor.search(params[:name]).first
+      ActorMovie.create!(movie_id: params[:id], actor_id: @actor.id)
+      redirect_to "/movies/#{params[:id]}"
+    end
   end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe 'movies show page' do
     @movie_1 = @studio_1.movies.create!(title: "True Lies", creation_year: 1995, genre: "Action")
     @movie_2 = @studio_1.movies.create!(title: "Terminator", creation_year: 1990, genre: "Action")
     @movie_3 = @studio_2.movies.create!(title: "Skyfall", creation_year: 2019, genre: "Action")
-    @movie_4 = @studio_3.movies.create!(title: "Spectre", creation_year: 2020, genre: "Action")
+    @movie_4 = @studio_3.movies.create!(title: "Golden Eye", creation_year: 2008, genre: "Action")
 
     @actor_1 = @movie_1.actors.create!(name: "Arnold S.", age: 74)
     @actor_2 = @movie_1.actors.create!(name: "Jamie Lee Curtis", age: 62)
     @actor_3 = @movie_1.actors.create!(name: "Tom Arnold", age: 68)
+    @actor_4 = @movie_2.actors.create!(name: "Daniel Craig", age: 50)
+    @actor_5 = @movie_3.actors.create!(name: "Pierce Brosnan", age: 67)
 
     visit "movies/#{@movie_1.id}"
   end
@@ -31,6 +33,20 @@ RSpec.describe 'movies show page' do
 
     it 'shows the average age of all the movies actors' do
       expect(page).to have_content("Average Age of Actors: 68")
+    end
+
+    it 'does not include actors that arent in this movie' do
+      expect(page).to_not have_content(@actor_4.name)
+      expect(page).to_not have_content(@actor_5.name)
+    end
+
+    it 'has a form to add an actor to the movie' do
+      fill_in(:name, with: 'Pierce Brosnan')
+      fill_in(:age, with: 90)
+      click_button("Submit")
+
+      expect(current_path).to eq("/movies/#{@movie_1.id}")
+      expect(page).to have_content("Pierce Brosnan")
     end
   end
 end
