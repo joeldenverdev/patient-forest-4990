f0990fe1fd1747a616f09e6d01b6569b3d170976
Joel Grant <joelmgrant@me.com>
2021-12-14 13:54:42 -0700

    Tests: Create model relationship tests

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Actor, type: :model do
   describe 'relationships' do
-    it { should belong_to :studio }
+    it { should have_many(:actor_movies) }
+    it { should have_many(:movies).through(:actor_movies) }
   end
 end
