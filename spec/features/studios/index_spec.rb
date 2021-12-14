ffe6d4c0573e19c140bcaed8e7603b8ce4aa885b
Joel Grant <joelmgrant@me.com>
2021-12-14 14:22:21 -0700

    Add more studios index page tests and update view

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe 'studios index page' do
     @studio_1 = Studio.create!(name: "Colorado Studio", location: "Boulder, Colorado")
     @studio_2 = Studio.create!(name: "Colorado Studio", location: "Boulder, Colorado")
     @studio_3 = Studio.create!(name: "Colorado Studio", location: "Boulder, Colorado")
+    @movie_1 = @studio_1.movies.create!(title: "True Lies", creation_year: 1995, genre: "Action")
+    @movie_2 = @studio_1.movies.create!(title: "Terminator", creation_year: 1990, genre: "Action")
+    @movie_3 = @studio_2.movies.create!(title: "Skyfall", creation_year: 2019, genre: "Action")
+    @movie_4 = @studio_3.movies.create!(title: "Spectre", creation_year: 2020, genre: "Action")
   end
 
   describe 'when I visit the studios index page' do
@@ -14,6 +18,15 @@ RSpec.describe 'studios index page' do
       expect(page).to have_content(@studio_1.name)
       expect(page).to have_content(@studio_2.name)
       expect(page).to have_content(@studio_3.name)
+    end
+
+    it 'displays the movies which belong to each studio' do
+      visit "/studios"
+
+      expect(page).to have_content(@studio_1.movies[0].title)
+      expect(page).to have_content(@studio_1.movies[1].title)
+      expect(page).to have_content(@studio_2.movies[0].title)
+      expect(page).to have_content(@studio_3.movies[0].title)
     end
   end
 end
